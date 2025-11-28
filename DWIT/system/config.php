f56618fc8d42cdf4bbf3082c841ec238cccd8658
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-11-28 20:11:37 +0545

    config reversed

--- DWIT/system/config.php
+++ DWIT/system/config.php
@@ -1,10 +1,10 @@
 <?php
 define('APP_ROOT', dirname(dirname(__FILE__)));
 define('DB_SERVER','localhost');
-// define('DB_USER', 'dwit');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-// define('DB_PASSWORD', 'dAkbG5');
+define('DB_USER', 'dwit');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'dAkbG5');
 define('DB_NAME','newdwit');
 define('EMAIL','admission@deerwalk.edu.np');
 define('PASSWORD','nfoc evca vlew auuj');
